dbf1b0ad21b32b2d97bbb6cd967cd0bfcd5ff780
kkukyang <yangyk7364@gmail.com>
2020-04-01 04:03:27 +0900

    hp

---
+++ TamingGame/Assets/Scripts/HPBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//HPBar/HP 의 스케일을 조절하여 HP표시 100=초록 70=노랑 30=빨강
+public class HPBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
